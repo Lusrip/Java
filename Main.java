0d2d706122f157700173f4fcaba9e6c3416454fb
Lusrip <loced63@gmail.com>
2022-05-29 14:06:24 +0300

    full file code

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean symboll_1 = false;
+        String sym_1 = "";
+        Scanner input = new Scanner(System.in);
 
+        while (!symboll_1) {
+            System.out.print("Enter your symbol: ");
+            sym_1 = input.symboll_1();
+
+            if (sym_1() !="{[({[()]})]}")
+            {
+                System.out.println("Bad input");
+            }
+            else
+                symboll_1 = true;
+        }
+
+        System.out.println("Symbol is:" + sym_1);
     }
 }
