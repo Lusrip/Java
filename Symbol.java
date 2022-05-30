bcef43f2f58e2a562b2d868585bb38786ab3fc14
Lusrip <loced63@gmail.com>
2022-05-30 12:17:58 +0300

    Добавлены комментарии и также файл README.txt

--- Symbol.java
+++ Symbol.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Объявление переменных
         boolean symbol = false;
         String sym = "";
         Scanner input = new Scanner(System.in);
-
+        //Вывод сообщения о вводе символов
         while (!symbol) {
             System.out.print("Enter your symbol: ");
             sym = input.nextLine();
-
-            if (sym.length() !="{[({[()]})]}")
+            //Условаие проверки корректности введенных символов
+            if (sym() !="{[({[()]})]}")
             {
                 System.out.println("Bad input");
             }
             else
                 symbol = true;
         }
-
+        //Вывод вводимых символов
         System.out.println("Symbol is:" + sym);
     }
 }
